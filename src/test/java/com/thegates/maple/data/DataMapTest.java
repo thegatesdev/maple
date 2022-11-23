d5b37be61ccd4dd6f1f8ca57ff1bb559ae25c281
TheGatesScratch <mtkarels@outlook.com>
2022-11-23 12:34:31 +0100

    Add ifPresent tests.

--- src/test/java/com/thegates/maple/data/DataMapTest.java
+++ src/test/java/com/thegates/maple/data/DataMapTest.java
@@ -9,30 +9,9 @@ import java.util.concurrent.atomic.AtomicReference;
 
 class DataMapTest {
 
-    private static DataMap testMap;
-
-    @BeforeEach
-    @Test
-    void put() {
-        testMap = new DataMap();
-        testMap.put("map_entry", new DataMap().put("nested_entry_1", new DataMap().put("nested_entry_2", new DataPrimitive("test"))));
-        testMap.put("list_entry", new DataList().add(new DataPrimitive(new Object())));
-        testMap.put("null_entry", new DataNull());
-        testMap.put("primitive_entry", new DataPrimitive("test_2"));
-    }
-
-
-    @Test
-    void string() {
-        assert testMap.toString().equals("dataMap{'map_entry': dataMap{'nested_entry_1': dataMap{'nested_entry_2': dataPrimitive<String>}}, 'list_entry': dataList[dataPrimitive<Object>], 'null_entry': dataNull, 'primitive_entry': dataPrimitive<String>}");
-    }
-
-    @Test
-    void requireKeys_size() {
-        assert testMap.hasKeys("map_entry", "list_entry", "null_entry", "primitive_entry");
-        assert testMap.size() == 4;
-        assert testMap.get("map_entry").requireOf(DataMap.class).size() == 1;
-    }
+    private DataMap testMap;
+    private DataMap mapEntry;
+    private DataList listEntry;
 
     @Test
     void get_is() {
@@ -44,9 +23,26 @@ class DataMapTest {
 
     @Test
     void ifPresent() {
-        final AtomicReference<String> test = new AtomicReference<>();
-        testMap.ifPresent("primitive_entry", el -> test.set(el.asPrimitive().stringValue()));
-        assert test.get().equals("test_2");
+        final AtomicReference<String> ifPresent = new AtomicReference<>();
+        testMap.ifPresent("primitive_entry", el -> ifPresent.set(el.asPrimitive().stringValue()));
+        assert ifPresent.get().equals("test_2");
+
+        final AtomicReference<String> ifPrimitive = new AtomicReference<>();
+        testMap.ifPrimitive("primitive_entry", primitive -> ifPrimitive.set(primitive.stringValue()));
+        assert ifPrimitive.get().equals("test_2");
+
+        final AtomicReference<DataMap> ifMap = new AtomicReference<>();
+        testMap.ifMap("map_entry", ifMap::set);
+        assert ifMap.get() == mapEntry;
+
+        final AtomicReference<DataList> ifList = new AtomicReference<>();
+        testMap.ifList("list_entry", ifList::set);
+        assert ifList.get() == listEntry;
+    }
+
+    @Test
+    void name() {
+        assert testMap.get("map_entry").name().equals("map_entry");
     }
 
     @Test
@@ -59,9 +55,16 @@ class DataMapTest {
         assert testMap.get("map_entry").parent() == testMap;
     }
 
+    @BeforeEach
     @Test
-    void name() {
-        assert testMap.get("map_entry").name().equals("map_entry");
+    void put() {
+        testMap = new DataMap();
+        mapEntry = new DataMap().put("nested_entry_1", new DataMap().put("nested_entry_2", new DataPrimitive("test")));
+        testMap.put("map_entry", mapEntry);
+        listEntry = new DataList().add(new DataPrimitive(new Object()));
+        testMap.put("list_entry", listEntry);
+        testMap.put("null_entry", new DataNull());
+        testMap.put("primitive_entry", new DataPrimitive("test_2"));
     }
 
     @Test
@@ -70,6 +73,18 @@ class DataMapTest {
         assert read.hasKeys("1", "2");
         read.requireOf("1", DataPrimitive.class);
         read.requireOf("2", DataList.class);
+    }
+
+    @Test
+    void requireKeys_size() {
+        assert testMap.hasKeys("map_entry", "list_entry", "null_entry", "primitive_entry");
+        assert testMap.size() == 4;
+        assert testMap.get("map_entry").requireOf(DataMap.class).size() == 1;
+    }
+
+    @Test
+    void string() {
+        assert testMap.toString().equals("dataMap{'map_entry': dataMap{'nested_entry_1': dataMap{'nested_entry_2': dataPrimitive<String>}}, 'list_entry': dataList[dataPrimitive<Object>], 'null_entry': dataNull, 'primitive_entry': dataPrimitive<String>}");
     }
 
     @AfterEach
