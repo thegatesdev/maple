3302aaa4bf08bf8114eb850d8e84c76bb6b66563
TheGatesScratch <mtkarels@outlook.com>
2023-06-03 19:12:08 +0200

    Move specific getters to interface.

--- src/main/java/io/github/thegatesdev/maple/DataMap.java
+++ src/main/java/io/github/thegatesdev/maple/DataMap.java
@@ -1,7 +1,5 @@
 package io.github.thegatesdev.maple;
 
-import io.github.thegatesdev.maple.exception.ElementException;
-
 import java.util.Collections;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -9,7 +7,7 @@ import java.util.function.Consumer;
 /**
  * An element that maps String keys to DataElement values.
  */
-public class DataMap extends DataElement {
+public class DataMap extends DataElement implements MappedElements<String> {
 
     private final Map<String, DataElement> elements, view;
 
@@ -29,8 +27,8 @@ public class DataMap extends DataElement {
      * @return The previous element mapped to this key, or null if it wasn't present.
      */
     public DataElement set(String key, DataElement element) {
-        element.connect(this, key);
         var old = elements.put(key, element);
+        connectThis(element, key);
         if (old != null) old.disconnect();
         return old;
     }
@@ -66,251 +64,16 @@ public class DataMap extends DataElement {
      */
     public DataElement get(String key) {
         var el = getOrNull(key);
-        if (el == null) return new DataNull().connect(this, key);
+        if (el == null) return connectThis(new DataNull(), key);
         return el;
     }
 
-    // -- ELEMENT GETTERS
-
-
-    // ANY
-
-    /**
-     * Runs the action if the specified element is present, or the elseAction if not.
-     *
-     * @param key        The key to find the element at.
-     * @param action     The consumer to run when the element is found.
-     * @param elseAction The runnable to run when the element is not present.
-     * @return This DataMap.
-     */
-    public DataMap ifPresent(String key, Consumer<DataElement> action, Runnable elseAction) {
-        final DataElement el = getOrNull(key);
-        if (el != null) action.accept(el);
-        else if (elseAction != null) elseAction.run();
-        return this;
-    }
-
-    /**
-     * Runs the action if the specified element is present.
-     *
-     * @param key    The key to find the element at.
-     * @param action The consumer to run when the element is found.
-     * @return This DataMap.
-     */
-    public DataMap ifPresent(String key, Consumer<DataElement> action) {
-        return ifPresent(key, action, null);
-    }
-
-    // VALUE
-
-    /**
-     * Get the value element associated with this key.
-     *
-     * @param key The key associated with the value element.
-     * @return The found DataValue.
-     * @throws ElementException If the element was not found, or the element was not a value element.
-     */
-    public DataValue getValue(String key) throws ElementException {
-        return get(key).requireOf(DataValue.class);
-    }
-
-    /**
-     * Get the value element associated with this key, or a default.
-     *
-     * @param key The key associated with the value element.
-     * @param def The value to return if the element is not a value element, or is not present.
-     * @return The found DataValue, or the default value.
-     */
-    public DataValue getValue(String key, DataValue def) {
-        return get(key, DataValue.class, def);
-    }
-
-    /**
-     * Runs the action if the specified element is present and is a DataValue.
-     *
-     * @param key    The key to find the element at.
-     * @param action The consumer to run when the element is found.
-     * @return This DataMap.
-     */
-    public DataMap ifValue(String key, Consumer<DataValue> action) {
-        return ifValue(key, action, null);
-    }
-
-    /**
-     * Runs the action if the specified element is present and is a DataValue, or the elseAction if not.
-     *
-     * @param key        The key to find the element at.
-     * @param action     The consumer to run when the element is found.
-     * @param elseAction The runnable to run when the element is not present or not a DataValue.
-     * @return This DataMap.
-     */
-    public DataMap ifValue(String key, Consumer<DataValue> action, Runnable elseAction) {
-        final DataElement el = getOrNull(key);
-        if (el != null && el.isValue()) action.accept(el.asValue());
-        else if (elseAction != null) elseAction.run();
-        return this;
-    }
-
-    /**
-     * Get the value of the value element associated with this key, cast to P.
-     *
-     * @param key The key associated with the value element.
-     * @param <P> The type to cast to.
-     * @return The cast value.
-     * @throws ElementException If the element was not found, or is not a value element.
-     */
-    public <P> P getUnsafe(String key) throws ElementException {
-        return getValue(key).valueUnsafe();
-    }
-
-    /**
-     * Get the value of the value element associated with this key, cast to P, or a default value.
-     * This does not return the default value if the cast throws!
-     *
-     * @param key The key associated with the primitive element.
-     * @param <P> The type to cast to.
-     * @param def The default value to return if the element is not a value element or is not present.
-     * @return The cast value.
-     */
-    public <P> P getUnsafe(String key, P def) {
-        final DataElement el = getOrNull(key);
-        if (el == null || !el.isValue()) return def;
-        final P val = el.asValue().valueUnsafe();
-        return def == null ? val : (val == null ? def : val);
-    }
-
-    /**
-     * Get the value of the value element associated with this key, or throw.
-     *
-     * @param key            The key of the value element.
-     * @param primitiveClass The class the value should be of.
-     * @param <P>            The type the value should be of.
-     * @return The value of the value element.
-     * @throws ElementException If the value element was not found, or the value did not conform to P.
-     */
-    public <P> P get(String key, Class<P> primitiveClass) throws ElementException {
-        return getValue(key).valueOrThrow(primitiveClass);
-    }
-
-    /**
-     * Get the value of the value element associated with this key, or a default.
-     *
-     * @param key            The key of the value element.
-     * @param primitiveClass The class the value should be of.
-     * @param <P>            The type the value should be of.
-     * @param def            The default value to return when the element is not present, not a value element, or the type does not match.
-     * @return The value of the value element, or the default value.
-     */
-    public <P> P get(String key, Class<P> primitiveClass, P def) {
-        final DataElement el = getOrNull(key);
-        if (el == null || !el.isValue()) return def;
-        final P val = el.asValue().valueOrNull(primitiveClass);
-        return def == null ? val : (val == null ? def : val);
-    }
-
-
-    // MAP
-
-    /**
-     * Get the map element associated with this key.
-     *
-     * @param key The key associated with the map element.
-     * @return The found DataMap.
-     * @throws ElementException If the element was not found, or the element was not a map element.
-     */
-    public DataMap getMap(String key) throws ElementException {
-        return get(key).requireOf(DataMap.class);
-    }
-
-    /**
-     * Get the map element associated with this key, or a default.
-     *
-     * @param key The key associated with the map element.
-     * @param def The value to return if the element is not a map element, or is not present.
-     * @return The found DataMap, or the default value.
-     */
-    public DataMap getMap(String key, DataMap def) {
-        return get(key, DataMap.class, def);
-    }
-
-    /**
-     * Runs the action if the specified element is present and is a DataMap.
-     *
-     * @param key    The key to find the element at.
-     * @param action The consumer to run when the element is found.
-     * @return This DataMap.
-     */
-    public DataMap ifMap(String key, Consumer<DataMap> action) {
-        return ifMap(key, action, null);
-    }
-
-    /**
-     * Runs the action if the specified element is present and is a DataMap, or the elseAction if not.
-     *
-     * @param key        The key to find the element at.
-     * @param action     The consumer to run when the element is found.
-     * @param elseAction The runnable to run when the element is not present or not a DataMap.
-     * @return This DataMap.
-     */
-    public DataMap ifMap(String key, Consumer<DataMap> action, Runnable elseAction) {
-        final DataElement el = getOrNull(key);
-        if (el != null && el.isMap()) action.accept(el.asMap());
-        else if (elseAction != null) elseAction.run();
-        return this;
-    }
-
-    // LIST
-
-    /**
-     * Get the list element associated with this key.
-     *
-     * @param key The key associated with the list element.
-     * @return The found DataList.
-     * @throws ElementException If the element was not found, or the element was not a list element.
-     */
-    public DataList getList(String key) throws ElementException {
-        return get(key).requireOf(DataList.class);
-    }
-
-    /**
-     * Get the list element associated with this key, or a default.
-     *
-     * @param key The key associated with the list element.
-     * @param def The value to return if the element is not a list element, or is not present.
-     * @return The found DataList, or the default value.
-     */
-    public DataList getList(String key, DataList def) {
-        return get(key, DataList.class, def);
-    }
-
-    /**
-     * Runs the action if the specified element is present and is a DataList.
-     *
-     * @param key    The key to find the element at.
-     * @param action The consumer to run when the element is found.
-     * @return This DataMap.
-     */
-    public DataMap ifList(String key, Consumer<DataList> action) {
-        return ifList(key, action, null);
-    }
-
-    /**
-     * Runs the action if the specified element is present and is a DataList, or the elseAction if not.
-     *
-     * @param key        The key to find the element at.
-     * @param action     The consumer to run when the element is found.
-     * @param elseAction The runnable to run when the element is not present or not a DataList.
-     * @return This DataMap.
-     */
-    public DataMap ifList(String key, Consumer<DataList> action, Runnable elseAction) {
-        final DataElement el = getOrNull(key);
-        if (el != null && el.isMap()) action.accept(el.asList());
-        else if (elseAction != null) elseAction.run();
-        return this;
-    }
 
     // -- ELEMENT
 
+    private DataElement connectThis(DataElement element, String key) {
+        return element.connect(this, key);
+    }
 
     @Override
     public boolean isMap() {
