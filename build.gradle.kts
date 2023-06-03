9d3e9e42cae321166b58bfe91cd900e1f5af5620
TheGatesScratch <mtkarels@outlook.com>
2023-06-03 15:17:31 +0200

    Switch to Gradle build system.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    `java-library`
+    java
+    idea
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+group = "io.github.thegatesdev"
+version = "1.4.2"
+description = "A clean type safe data structure."
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+java {
+    withSourcesJar()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
