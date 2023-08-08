6925b04523781c2a97ba9a27e1ebd210efe18dbe
TheGatesScratch <mtkarels@outlook.com>
2023-08-08 08:30:55 +0200

    Simplify map options reading.

---
+++ src/main/java/io/github/thegatesdev/maple/read/Options.java
@@ -0,0 +1,95 @@
+package io.github.thegatesdev.maple.read;
+
+import io.github.thegatesdev.maple.Maple;
+import io.github.thegatesdev.maple.data.DataElement;
+import io.github.thegatesdev.maple.data.DataMap;
+import io.github.thegatesdev.maple.data.DataValue;
+import io.github.thegatesdev.maple.exception.ElementException;
+import io.github.thegatesdev.maple.read.struct.DataType;
+import io.github.thegatesdev.maple.read.struct.DataTypeHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Options {
+
+    private final List<Option<?>> entries = new ArrayList<>(1);
+
+
+    private static Object readEntry(DataMap input, DataElement el, Option<?> entry) {
+        if (el != null) return entry.dataType.read(el); // Present
+        if (!entry.hasDefault) throw ElementException.requireField(input, entry.key); // Not present and no default! Error!
+        return entry.defaultValue; // We have a default! Phew..
+    }
+
+
+    public static DataMap read(Options options, DataMap input, DataMap output) {
+        try {
+            for (var entry : options.entries) output.set(entry.key, readEntry(input, input.getOrNull(entry.key), entry));
+        } catch (ElementException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ElementException(input, "Error while reading options: " + e.getMessage(), e);
+        }
+        return output;
+    }
+
+    public static DataMap read(Options options, DataMap input) {
+        return read(options, input, new DataMap(input.size()));
+    }
+
+    public static DataMap transform(Options options, DataMap data) {
+        try {
+            for (var entry : options.entries) data.change(entry.key, el -> Maple.read(readEntry(data, el, entry)));
+        } catch (ElementException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ElementException(data, "Error while reading options: " + e.getMessage(), e);
+        }
+        return data;
+    }
+
+
+    public Options add(String key, DataTypeHolder<?> holder) {
+        return add(new Option<>(key, holder.dataType()));
+    }
+
+    public Options optional(String key, DataTypeHolder<?> holder) {
+        return add(new Option<>(key, holder.dataType(), null));
+    }
+
+    public <Type> Options add(String key, DataTypeHolder<Type> holder, Type def) {
+        return add(new Option<>(key, holder.dataType(), Objects.requireNonNull(def)));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <Val, Type extends DataValue<Val>> Options addVal(String key, DataTypeHolder<Type> holder, Val def) {
+        return add(key, ((DataType<DataValue<Val>>) holder.dataType()), DataValue.of(def));
+    }
+
+
+    private Options add(Option<?> entry) {
+        entries.add(entry);
+        return this;
+    }
+
+
+    public record Option<Type>(String key,
+                               DataType<Type> dataType,
+                               Type defaultValue,
+                               boolean hasDefault) {
+        public Option {
+            Objects.requireNonNull(key);
+            Objects.requireNonNull(dataType);
+        }
+
+        public Option(String key, DataType<Type> dataType, Type defaultValue) {
+            this(key, dataType, defaultValue, true);
+        }
+
+        public Option(String key, DataType<Type> dataType) {
+            this(key, dataType, null, false);
+        }
+    }
+}
