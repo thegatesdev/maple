62701e3ba062ed9e33c48066b990037e8f20820e
thegatesdev <mtkarels@outlook.com>
2023-10-26 21:25:13 +0200

    DataElement javadocs

--- src/main/java/io/github/thegatesdev/maple/element/DataElement.java
+++ src/main/java/io/github/thegatesdev/maple/element/DataElement.java
@@ -21,74 +21,134 @@ import io.github.thegatesdev.maple.exception.ElementTypeException;
 
 import java.util.function.Consumer;
 
+/**
+ * An interface defining the base functionality of an element in a Maple structure.
+ */
 public sealed interface DataElement permits DataList, DataMap, DataValue {
 
     // Self
 
+    /**
+     * Deep copy this element to a new element of the same type.
+     * All descendants will be copied as well, excluding any value elements, as they are immutable.
+     *
+     * @return a new element holding the same structure
+     */
     DataElement structureCopy();
 
     // Value
 
+    /**
+     * @return {@code true} if the element does not contain any value
+     */
     boolean isEmpty();
 
+    /**
+     * @return {@code true} if the element contains any value
+     */
     default boolean isPresent() {
         return !isEmpty();
     }
 
     // Type
 
+    /**
+     * @return {@code true} if this element is a map element
+     */
     default boolean isMap() {
         return false;
     }
 
+    /**
+     * @return {@code true} if this element is a list element
+     */
     default boolean isList() {
         return false;
     }
 
+    /**
+     * @return {@code true} if this element is a value element
+     */
     default boolean isValue() {
         return false;
     }
 
 
+    /**
+     * @return the corresponding element type for this element
+     */
     ElementType getType();
 
 
+    /**
+     * @return this element, as a map element
+     * @throws ElementTypeException if this element is not a map
+     */
     default DataMap asMap() throws ElementTypeException {
         throw new ElementTypeException(ElementType.MAP, getType());
     }
 
+    /**
+     * @return this element, as a list element
+     * @throws ElementTypeException if this element is not a list
+     */
     default DataList asList() throws ElementTypeException {
         throw new ElementTypeException(ElementType.LIST, getType());
     }
 
+    /**
+     * @return this element, as a value element
+     * @throws ElementTypeException if this element is not a value
+     */
     default DataValue<?> asValue() throws ElementTypeException {
         throw new ElementTypeException(ElementType.VALUE, getType());
     }
 
 
+    /**
+     * Run the given action if this element is a map.
+     * Run the given else-action if this element is not a map.
+     */
     default void ifMap(Consumer<DataMap> action, Runnable elseAction) {
         if (isMap()) action.accept(asMap());
         else if (elseAction != null) elseAction.run();
     }
 
+    /**
+     * Run the given action if this element is a map.
+     */
     default void ifMap(Consumer<DataMap> action) {
         ifMap(action, null);
     }
 
+    /**
+     * Run the given action if this element is a list.
+     * Run the given else-action if this element is not a list.
+     */
     default void ifList(Consumer<DataList> action, Runnable elseAction) {
         if (isList()) action.accept(asList());
         else if (elseAction != null) elseAction.run();
     }
 
+    /**
+     * Run the given action if this element is a list.
+     */
     default void ifList(Consumer<DataList> action) {
         ifList(action, null);
     }
 
+    /**
+     * Run the given action if this element is a value.
+     * Run the given else-action if this element is not a value.
+     */
     default void ifValue(Consumer<DataValue<?>> action, Runnable elseAction) {
         if (isValue()) action.accept(asValue());
         else if (elseAction != null) elseAction.run();
     }
 
+    /**
+     * Run the given action if this element is a value.
+     */
     default void ifValue(Consumer<DataValue<?>> action) {
         ifValue(action, null);
     }
