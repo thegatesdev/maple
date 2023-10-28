550096267a351e4fce31c3bfe8a655e01bbf0672
thegatesdev <mtkarels@outlook.com>
2023-10-28 10:30:03 +0200

    DefaultConversion javadoc

--- src/main/java/io/github/thegatesdev/maple/conversion/DefaultConversion.java
+++ src/main/java/io/github/thegatesdev/maple/conversion/DefaultConversion.java
@@ -9,15 +9,29 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public final class DefaultConversion implements Conversion{
+/**
+ * This conversion is able to convert most existing Java types to their element equivalent.
+ * Arrays, Lists, and optionally Iterables will be converted to {@link DataList}.
+ * Maps will be converted to {@link DataMap}.
+ * The remaining types will be wrapped in a {@link StaticDataValue}.
+ */
+public final class DefaultConversion implements Conversion {
 
-    private boolean useToStringKeys = false, convertIterable = false;
+    private boolean useToStringKeys = false, convertIterable = true;
 
+    /**
+     * Indicate to use {@link Object#toString()} to convert and use potential non-String keys, instead of ignoring them.
+     * Default is {@code false}.
+     */
     public DefaultConversion useToStringKeys(boolean useToStringKeys) {
         this.useToStringKeys = useToStringKeys;
         return this;
     }
 
+    /**
+     * Indicate to convert {@link Iterable} entries to their {@link DataList} equivalent, instead of wrapping them in a {@link StaticDataValue}.
+     * Default is {@code true}.
+     */
     public DefaultConversion convertIterable(boolean convertIterable) {
         this.convertIterable = convertIterable;
         return this;
@@ -29,7 +43,7 @@ public final class DefaultConversion implements Conversion{
         if (object instanceof DataElement) throw new IllegalArgumentException("Cannot convert 'DataElement'");
 
         if (object instanceof Object[] someArray) return convertList(List.of(someArray));
-        if (object instanceof Map<?,?> someMap) return convertMap(someMap);
+        if (object instanceof Map<?, ?> someMap) return convertMap(someMap);
         if (object instanceof List<?> someList) return convertList(someList);
         if (convertIterable && object instanceof Iterable<?> someIterable) return convertList(List.of(someIterable));
 
