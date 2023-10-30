088997e17986a1e5e5ad1cc53308b8b44fd94bbf
thegatesdev <mtkarels@outlook.com>
2023-10-30 18:47:57 +0100

    Use java-library toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     java
     `maven-publish`
+    `java-library`
 }
 
 group = "io.github.thegatesdev"
 version = "4.0.0"
 description = "A clean, type safe configuration structure."
-java.sourceCompatibility = JavaVersion.VERSION_17
 
 java {
     withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
