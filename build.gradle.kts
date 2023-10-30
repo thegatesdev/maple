7e2adc254b86bae755123f55444106dbd67b26a8
thegatesdev <mtkarels@outlook.com>
2023-10-30 18:53:42 +0100

    Add maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ group = "io.github.thegatesdev"
 version = "4.0.0"
 description = "A clean, type safe configuration structure."
 
+repositories {
+    mavenCentral()
+}
+
 java {
     withSourcesJar()
     toolchain {
