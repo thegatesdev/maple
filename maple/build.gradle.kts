e44855b49c508045a514d6a551f8f2f0402e6b74
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-01-17 19:38:13 +0100

    Initial setup

---
+++ maple/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "io.github.thegatesdev"
+version = "5.0.0-SNAPSHOT"
+
+java{
+    withSourcesJar()
+    withJavadocJar()
+
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+publishing.publications.create<MavenPublication>("mavenJava").from(components["java"])
