c696886da6dcde488e60ab521a8d320e73f954b7
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-01-20 15:07:35 +0100

    Add DictElement methods

--- maple/src/main/java/com/github/thegatesdev/maple/element/DictElement.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/DictElement.java
@@ -4,6 +4,12 @@ import com.github.thegatesdev.maple.element.impl.MemoryDictElement;
 
 public sealed interface DictElement extends Element, ElementCollection permits MemoryDictElement {
 
+
+    DictElement merged(DictElement other);
+
+    DictElement flattened();
+
+
     @Override
     default ElementType type() {
         return ElementType.DICTIONARY;
