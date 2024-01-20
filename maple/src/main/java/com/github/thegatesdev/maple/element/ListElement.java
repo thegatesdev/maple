bd036bcc16613313e328c83efff6575189a01eb8
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-01-20 15:07:40 +0100

    Add ListElement methods

--- maple/src/main/java/com/github/thegatesdev/maple/element/ListElement.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/ListElement.java
@@ -2,7 +2,17 @@ package com.github.thegatesdev.maple.element;
 
 import com.github.thegatesdev.maple.element.impl.MemoryListElement;
 
+import java.util.Optional;
+
 public sealed interface ListElement extends Element, ElementCollection permits MemoryListElement {
+
+    Element get(int index);
+
+    Optional<Element> find(int index);
+
+
+    ListElement merged(ListElement other);
+
 
     @Override
     default ElementType type() {
