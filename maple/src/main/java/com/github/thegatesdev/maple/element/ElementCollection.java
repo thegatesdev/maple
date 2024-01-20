7edc7dcc65472839777f82ad65cc9a05485829b0
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-01-20 15:04:52 +0100

    Add ElementCollection methods

--- maple/src/main/java/com/github/thegatesdev/maple/element/ElementCollection.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/ElementCollection.java
@@ -1,4 +1,43 @@
 package com.github.thegatesdev.maple.element;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public sealed interface ElementCollection permits DictElement, ListElement {
+
+    char DEFAULT_DELIMITER = '.';
+
+
+    Element get(String path, char delimiter);
+
+    Optional<Element> find(String path, char delimiter);
+
+    default Element get(String path) {
+        return get(path, DEFAULT_DELIMITER);
+    }
+
+    default Optional<Element> find(String path) {
+        return find(path, DEFAULT_DELIMITER);
+    }
+
+
+    void each(Consumer<Element> action);
+
+    void crawl(Consumer<Element> action);
+
+    ElementCollection each(Function<Element, Element> transformer);
+
+    ElementCollection crawl(Function<Element, Element> transformer);
+
+
+    Stream<Element> toStream();
+
+    ListElement values();
+
+    ListElement values(Function<Element, Element> transformer);
+
+
+    int count();
 }
