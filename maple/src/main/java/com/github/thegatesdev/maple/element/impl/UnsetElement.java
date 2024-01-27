6bb4d60eafc4cffe0c7e80ad64d8679ff969bcd0
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-01-27 12:27:53 +0100

    Make UnsetElement a singleton

--- maple/src/main/java/com/github/thegatesdev/maple/element/impl/UnsetElement.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/impl/UnsetElement.java
@@ -3,7 +3,8 @@ package com.github.thegatesdev.maple.element.impl;
 import com.github.thegatesdev.maple.element.Element;
 import com.github.thegatesdev.maple.element.ElementType;
 
-public final class UnsetElement implements Element {
+public enum UnsetElement implements Element {
+    INSTANCE;
 
     @Override
     public boolean isUnset() {
