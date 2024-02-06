e6f0a36b65ac6cc9de4b72b7c7a2dddd4b86d763
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-06 17:28:01 +0100

    Element class javadocs

--- maple/src/main/java/com/github/thegatesdev/maple/element/Element.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/Element.java
@@ -6,105 +6,252 @@ import com.github.thegatesdev.maple.exception.ElementTypeException;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * A single element in a structure.
+ */
 public sealed interface Element permits DictElement, ElementCollection, ListElement, BoolElement, DoubleElement, FloatElement, IntElement, LongElement, StringElement, UnsetElement {
 
+    /**
+     * Get a list element containing the values from the supplied array.
+     *
+     * @param values the values for the list
+     * @return the list element containing the values
+     */
     static ListElement of(Element[] values) {
         return MemoryListElement.of(values);
     }
 
+    /**
+     * Get a list element containing the values from the supplied collection.
+     *
+     * @param values the values for the list
+     * @return the list element containing the values
+     */
     static ListElement of(Collection<Element> values) {
         return MemoryListElement.of(values);
     }
 
+    /**
+     * Get a dictionary element containing the keys and values from the supplied map.
+     *
+     * @param values the values for the dictionary
+     * @return the dictionary element containing the values
+     */
     static DictElement of(Map<String, Element> values) {
         return MemoryDictElement.of(values);
     }
 
+    /**
+     * Get an element containing the supplied string value.
+     *
+     * @param value the string value for the element
+     * @return the element containing the value
+     */
     static Element of(String value) {
         return StringElement.of(value);
     }
 
+    /**
+     * Get an element containing the supplied boolean value.
+     *
+     * @param value the boolean value for the element
+     * @return the element containing the value
+     */
     static Element of(boolean value) {
         return BoolElement.of(value);
     }
 
+    /**
+     * Get an element containing the supplied int value.
+     *
+     * @param value the int value for the element
+     * @return the element containing the value
+     */
     static Element of(int value) {
         return DoubleElement.of(value);
     }
 
+    /**
+     * Get an element containing the supplied long value.
+     *
+     * @param value the long value for the element
+     * @return the element containing the value
+     */
     static Element of(long value) {
         return LongElement.of(value);
     }
 
+    /**
+     * Get an element containing the supplied float value.
+     *
+     * @param value the float value for the element
+     * @return the element containing the value
+     */
     static Element of(float value) {
         return FloatElement.of(value);
     }
 
+    /**
+     * Get an element containing the supplied double value.
+     *
+     * @param value the double value for the element
+     * @return the element containing the value
+     */
     static Element of(double value) {
         return DoubleElement.of(value);
     }
 
+    /**
+     * Get an element containing no value.
+     * This is the equivalent of a 'null' value in the JSON specification.
+     *
+     * @return the element containing no value
+     */
     static Element ofUnset() {
         return UnsetElement.getInstance();
     }
 
 
+    /**
+     * Get the element type of this element.
+     *
+     * @return the type of this element
+     */
     ElementType type();
 
 
+    /**
+     * Check whether this element is a dictionary element.
+     *
+     * @return true if this element is a dictionary element
+     */
     default boolean isDict() {
         return false;
     }
 
+    /**
+     * Check whether this element is a list element.
+     *
+     * @return true if this element is a list element
+     */
     default boolean isList() {
         return false;
     }
 
+    /**
+     * Check whether this element contains a string value.
+     *
+     * @return true if this element contains a string value
+     */
     default boolean isString() {
         return false;
     }
 
+    /**
+     * Check whether this element contains a boolean value.
+     *
+     * @return true if this element contains a boolean value
+     */
     default boolean isBool() {
         return false;
     }
 
+    /**
+     * Check whether this element contains a number value.
+     *
+     * @return true if this element contains a number value
+     */
     default boolean isNumber() {
         return false;
     }
 
+    /**
+     * Check whether this element contains no value.
+     * This is the equivalent of a 'null' value in the JSON specification.
+     *
+     * @return true if this element contains no value
+     */
     default boolean isUnset() {
         return false;
     }
 
 
+    /**
+     * Get this element as a dictionary element, if applicable.
+     *
+     * @return this element
+     * @throws ElementTypeException if this element is not a dictionary element
+     */
     default DictElement getDict() {
         throw new ElementTypeException(ElementType.DICTIONARY, type());
     }
 
+    /**
+     * Get this element as a list element, if applicable.
+     *
+     * @return this element
+     * @throws ElementTypeException if this element is not a list element
+     */
     default ListElement getList() {
         throw new ElementTypeException(ElementType.LIST, type());
     }
 
+    /**
+     * Get the string value contained in this element, if applicable.
+     *
+     * @return the value from this element
+     * @throws ElementTypeException if this element did not contain a string value
+     */
     default String getString() {
         throw new ElementTypeException(ElementType.STRING, type());
     }
 
+    /**
+     * Get the boolean value contained in this element, if applicable.
+     *
+     * @return the value from this element
+     * @throws ElementTypeException if this element did not contain a boolean value
+     */
     default boolean getBool() {
         throw new ElementTypeException(ElementType.BOOLEAN, type());
     }
 
+    /**
+     * Get the int value contained in this element, if applicable.
+     *
+     * @return the value from this element
+     * @throws ElementTypeException if this element did not contain a int value
+     */
     default int getInt() {
         throw new ElementTypeException(ElementType.NUMBER, type());
     }
 
+    /**
+     * Get the long value contained in this element, if applicable.
+     *
+     * @return the value from this element
+     * @throws ElementTypeException if this element did not contain a long value
+     */
     default long getLong() {
         throw new ElementTypeException(ElementType.NUMBER, type());
     }
 
+    /**
+     * Get the float value contained in this element, if applicable.
+     *
+     * @return the value from this element
+     * @throws ElementTypeException if this element did not contain a float value
+     */
     default float getFloat() {
         throw new ElementTypeException(ElementType.NUMBER, type());
     }
 
+    /**
+     * Get the double value contained in this element, if applicable.
+     *
+     * @return the value from this element
+     * @throws ElementTypeException if this element did not contain a double value
+     */
     default double getDouble() {
         throw new ElementTypeException(ElementType.NUMBER, type());
     }
