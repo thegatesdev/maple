d3219dba1573f5b44beb1a4916584306ae0e4495
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-08 15:55:29 +0100

    Add Layout API package to module-export

--- maple/src/main/java/module-info.java
+++ maple/src/main/java/module-info.java
@@ -4,4 +4,5 @@
 module com.github.thegatesdev.maple {
     exports com.github.thegatesdev.maple.element;
     exports com.github.thegatesdev.maple.exception;
+    exports com.github.thegatesdev.maple.layout;
 }
