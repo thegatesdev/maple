f02b12bcc49ca062a259c47ee8106aa3f7df0f45
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-08 10:04:56 +0100

    ElementCollection class javadocs

--- maple/src/main/java/com/github/thegatesdev/maple/element/ElementCollection.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/ElementCollection.java
@@ -3,17 +3,47 @@ package com.github.thegatesdev.maple.element;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+/**
+ * Common methods for dictionary and list elements.
+ */
 public sealed interface ElementCollection extends Element permits DictElement, ListElement {
 
+    /**
+     * Iterate the values in this collection using the given action.
+     *
+     * @param action the action to run for each value
+     */
     void each(Consumer<Element> action);
 
+    /**
+     * Iterate the descendants in this collection using the given action.
+     * When an element collection is encountered, its values are crawled first.
+     *
+     * @param action the action to run for each descendant
+     */
     void crawl(Consumer<Element> action);
 
 
+    /**
+     * Get a stream of the values in this collection.
+     *
+     * @return a new stream with the values
+     */
     Stream<Element> stream();
 
+    /**
+     * Get a list element containing the values in this collection.
+     * If this element is already a list element, returns the same element.
+     *
+     * @return the list element with the values
+     */
     ListElement values();
 
 
+    /**
+     * Get the amount of items in this collection.
+     *
+     * @return the item count
+     */
     int count();
 }
