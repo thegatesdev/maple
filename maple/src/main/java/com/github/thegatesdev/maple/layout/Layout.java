6ca1bdd72b2c97482692e9edb503be0d61901da0
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-14 09:34:32 +0100

    Layout class javadocs

--- maple/src/main/java/com/github/thegatesdev/maple/layout/Layout.java
+++ maple/src/main/java/com/github/thegatesdev/maple/layout/Layout.java
@@ -2,12 +2,28 @@ package com.github.thegatesdev.maple.layout;
 
 import com.github.thegatesdev.maple.element.Element;
 
+/**
+ * Defines the layout expected of some element, and parses it to type T.
+ *
+ * @param <T> the type to parse to
+ */
 @FunctionalInterface
 public interface Layout<T> {
 
+    /**
+     * Get a new builder for constructing dictionary layouts.
+     *
+     * @return the new builder
+     */
     static DictLayout.Builder dictionary() {
         return new DictLayout.Builder();
     }
 
+    /**
+     * Parse the given value according to this layout.
+     *
+     * @param value the element to parse
+     * @return the value conforming to this layout
+     */
     T parse(Element value);
 }
