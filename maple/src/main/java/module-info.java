51fe35d476ca90138260f5ee5f4998c41568a518
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-14 09:59:50 +0100

    Module-export layout package

--- maple/src/main/java/module-info.java
+++ maple/src/main/java/module-info.java
@@ -3,5 +3,6 @@
  */
 module com.github.thegatesdev.maple {
     exports com.github.thegatesdev.maple.element;
+    exports com.github.thegatesdev.maple.layout;
     exports com.github.thegatesdev.maple.exception;
 }
