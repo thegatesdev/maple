4b222bfa7d9a432958b869660139ed8c171120c9
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-14 09:58:28 +0100

    DictLayout class javadocs

--- maple/src/main/java/com/github/thegatesdev/maple/layout/DictLayout.java
+++ maple/src/main/java/com/github/thegatesdev/maple/layout/DictLayout.java
@@ -6,6 +6,12 @@ import com.github.thegatesdev.maple.exception.ElementKeyNotPresentException;
 
 import java.util.*;
 
+/**
+ * A layout parsing a dictionary element to another dictionary element using the given options.
+ * An option tries to parse a layout at a specific key in the dictionary.
+ * Options can either be <i>required</i>; when the value is not present, an exception will be thrown,
+ * otherwise they are <i>optional</i>; when the value is not present, a given default will take its place.
+ */
 public final class DictLayout implements Layout<DictElement> {
 
     private final Option[] options;
@@ -33,6 +39,9 @@ public final class DictLayout implements Layout<DictElement> {
     }
 
 
+    /**
+     * Builder for creating a dictionary layout.
+     */
     public static final class Builder {
         private static final Layout<Element> IDENTITY_LAYOUT = value -> value;
 
@@ -42,23 +51,58 @@ public final class DictLayout implements Layout<DictElement> {
         Builder() {
         }
 
+        /**
+         * Create the dictionary layout from the options in the builder.
+         *
+         * @return the new dictionary layout
+         */
         public DictLayout build() {
             return new DictLayout(options.toArray(new Option[0]));
         }
 
 
+        /**
+         * Require a value at the given key, parsed with the given layout.
+         *
+         * @param key    the required key
+         * @param layout the parsed layout
+         * @return this builder
+         */
         public Builder require(String key, Layout<? extends Element> layout) {
             return add(key, layout, null);
         }
 
+        /**
+         * Require a value at the given key.
+         *
+         * @param key the required key
+         * @return this builder
+         */
         public Builder require(String key) {
             return require(key, IDENTITY_LAYOUT);
         }
 
+        /**
+         * Parse the value at the given key with the given layout, if it is present.
+         * Otherwise, place the given default value at that key.
+         *
+         * @param key          the key
+         * @param defaultValue the default value
+         * @param layout       the layout to parse
+         * @return this builder
+         */
         public Builder optional(String key, Element defaultValue, Layout<? extends Element> layout) {
             return add(key, layout, Objects.requireNonNull(defaultValue, "defaultValue cannot be null"));
         }
 
+        /**
+         * Keep the value at the given key if it is present.
+         * Otherwise, place the given default value at that key.
+         *
+         * @param key          the key
+         * @param defaultValue the default value
+         * @return this builder
+         */
         public Builder optional(String key, Element defaultValue) {
             return optional(key, defaultValue, IDENTITY_LAYOUT);
         }
