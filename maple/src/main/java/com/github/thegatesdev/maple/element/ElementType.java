964abcc4f85eea8be2a8047ee13e5891ac705efc
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-02-19 15:14:46 +0100

    Remove layout stuff

--- maple/src/main/java/com/github/thegatesdev/maple/element/ElementType.java
+++ maple/src/main/java/com/github/thegatesdev/maple/element/ElementType.java
@@ -1,13 +1,10 @@
 package com.github.thegatesdev.maple.element;
 
-import com.github.thegatesdev.maple.exception.LayoutParseException;
-import com.github.thegatesdev.maple.layout.Layout;
-
 /**
  * The different types of elements that are available.
  * These all represent some value from the JSON specification.
  */
-public enum ElementType implements Layout<Element> {
+public enum ElementType {
     /**
      * A dictionary, or mapping, of string keys to element values.
      * Equivalent of a JSON 'object'.
@@ -41,11 +38,4 @@ public enum ElementType implements Layout<Element> {
      * It is not advised to directly use this element type, it only exists to distinguish between absent and null values.
      */
     UNSET;
-
-    @Override
-    public Element parse(Element value) throws LayoutParseException {
-        if (this != value.type())
-            throw new LayoutParseException("Expected a %s element, got a %s element".formatted(this.name(), value.type().name()));
-        return value;
-    }
 }
