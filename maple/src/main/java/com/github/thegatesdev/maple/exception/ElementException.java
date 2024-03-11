398f6348f25cb8f35961c92c28040e55c1d5e5c4
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-03-11 09:21:12 +0100

    Add Apache 2.0 license notice

--- maple/src/main/java/com/github/thegatesdev/maple/exception/ElementException.java
+++ maple/src/main/java/com/github/thegatesdev/maple/exception/ElementException.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 Timar Karels
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.github.thegatesdev.maple.exception;
 
 /**
@@ -5,7 +21,8 @@ package com.github.thegatesdev.maple.exception;
  *
  * @author Timar Karels
  */
-public sealed class ElementException extends RuntimeException permits ElementKeyNotPresentException, ElementTypeException {
+public sealed class ElementException extends RuntimeException permits ElementKeyNotPresentException,
+        ElementTypeException {
 
     /**
      * Create the exception with the given message.
