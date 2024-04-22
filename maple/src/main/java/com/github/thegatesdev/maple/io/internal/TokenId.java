c9e978aca8c0cf24c196bc5d50fbb3ed34f91e17
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-04-22 17:49:35 +0200

    Rename token ids

--- maple/src/main/java/com/github/thegatesdev/maple/io/internal/TokenId.java
+++ maple/src/main/java/com/github/thegatesdev/maple/io/internal/TokenId.java
@@ -3,15 +3,18 @@ package com.github.thegatesdev.maple.io.internal;
 public interface TokenId {
     byte ID_NOT_AVAILABLE = -1;
     byte ID_NOT_PRESENT = 0;
-    byte ID_OBJECT_START = 1;
-    byte ID_OBJECT_END = 2;
-    byte ID_ARRAY_START = 3;
-    byte ID_ARRAY_END = 4;
-    byte ID_FIELD_NAME = 5;
-    byte ID_VALUE_STRING = 6;
-    byte ID_VALUE_INTEGER = 7;
-    byte ID_VALUE_DECIMAL = 8;
-    byte ID_VALUE_TRUE = 9;
-    byte ID_VALUE_FALSE = 10;
-    byte ID_VALUE_NULL = 11;
+
+    // Structural
+    byte ID_BEGIN_OBJECT = 1;
+    byte ID_BEGIN_ARRAY = 2;
+    byte ID_END_OBJECT = 3;
+    byte ID_END_ARRAY = 4;
+    byte ID_NAME_SEPARATOR = 5;
+    byte ID_VALUE_SEPARATOR = 6;
+    // Values
+    byte ID_VALUE_TRUE = 7;
+    byte ID_VALUE_FALSE = 8;
+    byte ID_VALUE_NULL = 9;
+    byte ID_VALUE_NUMBER = 10;
+    byte ID_VALUE_STRING = 11;
 }
