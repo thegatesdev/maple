7aba48db2c7d732e2ebe63f682c1b4301fd138d0
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-04-25 17:34:27 +0200

    Remove SnakeYAML adaption tests

--- maple/build.gradle.kts
+++ maple/build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.snakeyaml:snakeyaml-engine:2.7")
 }
 
 tasks{
