6239316a6881e17a5adccae322b82db018448fbd
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-04-27 11:41:59 +0200

    Add new packages to exports

--- maple/src/main/java/module-info.java
+++ maple/src/main/java/module-info.java
@@ -7,4 +7,5 @@ module com.github.thegatesdev.maple {
     exports com.github.thegatesdev.maple.element;
     exports com.github.thegatesdev.maple.element.impl;
     exports com.github.thegatesdev.maple.exception;
+    exports com.github.thegatesdev.maple.io;
 }
