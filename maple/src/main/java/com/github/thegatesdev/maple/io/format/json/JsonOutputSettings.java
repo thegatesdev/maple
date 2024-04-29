dc637f52c35e10c85d3c9e9cc46558c6e2df75e8
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-04-29 11:27:38 +0200

    Remove settings, reset (de)serializer

--- maple/src/main/java/com/github/thegatesdev/maple/io/format/json/JsonOutputSettings.java
+++
@@ -1,4 +0,0 @@
-package com.github.thegatesdev.maple.io.format.json;
-
-public final class JsonOutputSettings {
-}
--- maple/src/main/java/com/github/thegatesdev/maple/io/format/json/internal/JsonSerializer.java
+++ maple/src/main/java/com/github/thegatesdev/maple/io/format/json/internal/JsonSerializer.java
@@ -1,7 +1,6 @@
 package com.github.thegatesdev.maple.io.format.json.internal;
 
 import com.github.thegatesdev.maple.io.*;
-import com.github.thegatesdev.maple.io.format.json.*;
 
 import java.io.*;
 import java.math.*;
@@ -9,106 +8,75 @@ import java.math.*;
 public final class JsonSerializer implements Serializer {
 
     private final Writer writer;
-    private final JsonOutputSettings settings;
 
-    public JsonSerializer(Writer writer, JsonOutputSettings settings) {
+    public JsonSerializer(Writer writer) {
         this.writer = writer;
-        this.settings = settings;
-    }
-
-
-    private void openStructure(int openCharacter) {
-        writeOrThrow(openCharacter);
-    }
-
-    private void closeStructure(int closeCharacter) {
-        writeOrThrow(closeCharacter);
-    }
-
-    private void writePlainValue(String value) {
-        writeOrThrow(value);
-    }
-
-    private void writeOrThrow(int value) {
-        try {
-            writer.write(value);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void writeOrThrow(String value) {
-        try {
-            writer.write(value);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
 
     @Override
     public void openObject() {
-        openStructure('{');
+
     }
 
     @Override
     public void closeObject() {
-        closeStructure('}');
+
     }
 
     @Override
     public void openArray() {
-        openStructure('[');
+
     }
 
     @Override
     public void closeArray() {
-        closeStructure(']');
+
     }
 
-    
+
     @Override
     public void name(String name) {
-        writeOrThrow('"' + name + '"' + ':');
+
     }
 
     @Override
     public void value(String value) {
-        writePlainValue('"' + value + '"');
+
     }
 
     @Override
     public void value(boolean value) {
-        writePlainValue(Boolean.toString(value));
+
     }
 
     @Override
     public void value(int value) {
-        writePlainValue(Integer.toString(value));
+
     }
 
     @Override
     public void value(long value) {
-        writePlainValue(Long.toString(value));
+
     }
 
     @Override
     public void value(float value) {
-        writePlainValue(Float.toString(value));
+
     }
 
     @Override
     public void value(double value) {
-        writePlainValue(Double.toString(value));
+
     }
 
     @Override
     public void value(BigInteger value) {
-        writePlainValue(value.toString());
+
     }
 
     @Override
     public void value(BigDecimal value) {
-        writePlainValue(value.toString());
+
     }
 }
