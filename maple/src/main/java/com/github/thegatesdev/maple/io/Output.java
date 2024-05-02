7a7498346fdf25a53e6aaca023175eb9741ba565
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-05-02 14:38:04 +0200

    Add OutputException type

--- maple/src/main/java/com/github/thegatesdev/maple/io/Output.java
+++ maple/src/main/java/com/github/thegatesdev/maple/io/Output.java
@@ -1,53 +1,60 @@
 package com.github.thegatesdev.maple.io;
 
+import com.github.thegatesdev.maple.exception.*;
+
 import java.math.*;
 
 public interface Output {
 
-    void write(int c);
+    void write(int c) throws OutputException;
 
-    void write(String s);
+    void write(char[] buf, int off, int len) throws OutputException;
 
-    void write(char[] buf, int off, int len);
+    void write(String s) throws OutputException;
 
-    void writeEscaped(char[] buf, int off, int len, int[] escapes);
+    default void write(char[] buf) throws OutputException {
+        write(buf, 0, buf.length);
+    }
 
-    void writeEscaped(String s, int[] escapes);
+
+    void writeEscaped(char[] buf, int off, int len, int[] escapes) throws OutputException;
+
+    void writeEscaped(String s, int[] escapes) throws OutputException;
 
 
-    default void writeValue(boolean b) {
+    default void writeValue(boolean b) throws OutputException {
         write(Boolean.toString(b));
     }
 
-    default void writeValue(int i) {
+    default void writeValue(int i) throws OutputException {
         write(Integer.toString(i));
     }
 
-    default void writeValue(long l) {
+    default void writeValue(long l) throws OutputException {
         write(Long.toString(l));
     }
 
-    default void writeValue(short s) {
+    default void writeValue(short s) throws OutputException {
         write(Short.toString(s));
     }
 
-    default void writeValue(byte b) {
+    default void writeValue(byte b) throws OutputException {
         write(Byte.toString(b));
     }
 
-    default void writeValue(float f) {
+    default void writeValue(float f) throws OutputException {
         write(Float.toString(f));
     }
 
-    default void writeValue(double d) {
+    default void writeValue(double d) throws OutputException {
         write(Double.toString(d));
     }
 
-    default void writeValue(BigInteger bi) {
+    default void writeValue(BigInteger bi) throws OutputException {
         write(bi.toString(10));
     }
 
-    default void writeValue(BigDecimal bd) {
+    default void writeValue(BigDecimal bd) throws OutputException {
         write(bd.toString());
     }
 }
