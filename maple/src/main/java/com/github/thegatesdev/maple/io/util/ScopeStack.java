6fab772b21cb6035621bbd254df2777fc6ab9584
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-05-04 20:27:49 +0200

    Clean up ScopeStack

--- maple/src/main/java/com/github/thegatesdev/maple/io/util/ScopeStack.java
+++ maple/src/main/java/com/github/thegatesdev/maple/io/util/ScopeStack.java
@@ -7,16 +7,20 @@ public final class ScopeStack {
     public static final byte STATUS_OK = 0;
     public static final byte STATUS_NEEDS_VALUE_SEPARATOR = 1;
     public static final byte STATUS_NEEDS_NAME_SEPARATOR = 2;
+    public static final byte STATUS_NEEDS_ROOT_SEPARATOR = 3;
     public static final byte STATUS_EXPECT_NAME = 4;
     public static final byte STATUS_EXPECT_VALUE = 5;
 
-    private int stackIndex = -1;
+    private static final byte SCOPE_ROOT = 0;
+    private static final byte SCOPE_OBJECT = 1;
+    private static final byte SCOPE_ARRAY = 2;
 
+    private int stackIndex = -1;
     private boolean[] scopes = new boolean[2];
 
-    private boolean currentScope = true;
+    private byte currentScopeID = SCOPE_ROOT;
     private boolean hasEntry = false;
-    private boolean nameWritten = false;
+    private boolean hasName = false;
 
 
     private void push(boolean scope) {
@@ -25,16 +29,36 @@ public final class ScopeStack {
             len *= 2;
             scopes = Arrays.copyOf(scopes, len);
         }
-        scopes[stackIndex] = currentScope = scope;
+        scopes[stackIndex] = scope;
+        currentScopeID = toId(scope);
         hasEntry = false; // New scope, is empty
     }
 
     private void pop() {
-        if (stackIndex != 0) {
+        if (stackIndex > 0) {
             stackIndex--;
-            currentScope = scopes[stackIndex];
+            currentScopeID = toId(scopes[stackIndex]);
+        } else {
+            currentScopeID = SCOPE_ROOT;
         }
         hasEntry = true; // Has at least 1 entry, since we just popped out of that one
+    }
+
+    private byte toId(boolean scope) {
+        return scope ? SCOPE_OBJECT : SCOPE_ARRAY;
+    }
+
+
+    public boolean inRoot() {
+        return currentScopeID == SCOPE_ROOT;
+    }
+
+    public boolean inObject() {
+        return currentScopeID == SCOPE_OBJECT;
+    }
+
+    public boolean inArray() {
+        return currentScopeID == SCOPE_ARRAY;
     }
 
 
@@ -58,37 +82,32 @@ public final class ScopeStack {
         return true;
     }
 
-    public boolean inArray() {
-        return !currentScope;
-    }
-
-    public boolean inObject() {
-        return currentScope;
-    }
-
 
     public byte writeValueStatus() {
-        if (stackIndex < 0) { // root, we don't handle that yet
-            return STATUS_OK; // Don't throw, this needs to be called in root for the initial root object
+        switch (currentScopeID) {
+            case SCOPE_ROOT -> {
+                boolean had = hasEntry;
+                hasEntry = true;
+                return had ? STATUS_NEEDS_ROOT_SEPARATOR : STATUS_OK;
+            }
+            case SCOPE_OBJECT -> {
+                if (!hasName) return STATUS_EXPECT_NAME;
+                hasName = false;
+                hasEntry = true;
+                return STATUS_NEEDS_NAME_SEPARATOR;
+            }
+            case SCOPE_ARRAY -> {
+                boolean had = hasEntry;
+                hasEntry = true;
+                return had ? STATUS_NEEDS_VALUE_SEPARATOR : STATUS_OK;
+            }
         }
-        if (inObject()) { // Object
-            if (!nameWritten) return STATUS_EXPECT_NAME;
-            nameWritten = false;
-            hasEntry = true;
-            return STATUS_NEEDS_NAME_SEPARATOR;
-        } else { // Array
-            boolean had = hasEntry;
-            hasEntry = true;
-            return had ? STATUS_NEEDS_VALUE_SEPARATOR : STATUS_OK;
-        }
+        return STATUS_OK;
     }
 
     public byte writeNameStatus() {
-        if (stackIndex < 0) { // root, we don't handle that yet
-            throw new IllegalStateException("root");
-        }
-        if (inArray() || nameWritten) return STATUS_EXPECT_VALUE;
-        nameWritten = true;
+        if (!inObject() || hasName) return STATUS_EXPECT_VALUE;
+        hasName = true;
         return hasEntry ? STATUS_NEEDS_VALUE_SEPARATOR : STATUS_OK;
     }
 }
