a18c5e4ffd530d40644e4854a46f6f76ae3f7690
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-05-20 17:49:25 +0200

    Use bitset for scopestack

--- maple/src/main/java/com/github/thegatesdev/maple/io/util/ScopeStack.java
+++ maple/src/main/java/com/github/thegatesdev/maple/io/util/ScopeStack.java
@@ -16,20 +16,38 @@ public final class ScopeStack {
     private static final byte SCOPE_ARRAY = 2;
 
     private int stackIndex = -1;
-    private boolean[] scopes = new boolean[2];
+    /**
+     * Scopes represented as a BitSet.
+     * A scope is either an array or an object.
+     * The root scope only appears before the start of the stack.
+     */
+    private BitSet scopes = new BitSet();
 
     private byte currentScopeID = SCOPE_ROOT;
     private boolean hasEntry = false;
-    private boolean hasName = false;
+    private boolean hasName = false; // Only in object scope, expect a value after a name
+
+
+    private void ensureCapacity(int index) {
+        int size = scopes.size();
+        if (index >= size) { // Full, double the capacity
+            size *= 2;
+            BitSet result = new BitSet(size);
+            result.or(scopes);
+            scopes = result;
+        }
+    }
+
+    private void setScope(int index, boolean scope) {
+        if (scope) scopes.set(stackIndex);
+        else scopes.clear(stackIndex);
+    }
 
 
     private void push(boolean scope) {
-        int len = scopes.length;
-        if (++stackIndex == len) { // Full, double the capacity
-            len *= 2;
-            scopes = Arrays.copyOf(scopes, len);
-        }
-        scopes[stackIndex] = scope;
+        ensureCapacity(++stackIndex);
+        setScope(stackIndex, scope);
+
         currentScopeID = toId(scope);
         hasEntry = false; // New scope, is empty
     }
@@ -37,7 +55,7 @@ public final class ScopeStack {
     private void pop() {
         if (stackIndex > 0) {
             stackIndex--;
-            currentScopeID = toId(scopes[stackIndex]);
+            currentScopeID = toId(scopes.get(stackIndex));
         } else {
             currentScopeID = SCOPE_ROOT;
         }
@@ -84,25 +102,23 @@ public final class ScopeStack {
 
 
     public byte writeValueStatus() {
-        switch (currentScopeID) {
-            case SCOPE_ROOT -> {
-                boolean had = hasEntry;
+        return switch (currentScopeID) {
+            case SCOPE_ROOT:
+                if (hasEntry) yield STATUS_NEEDS_ROOT_SEPARATOR;
                 hasEntry = true;
-                return had ? STATUS_NEEDS_ROOT_SEPARATOR : STATUS_OK;
-            }
-            case SCOPE_OBJECT -> {
-                if (!hasName) return STATUS_EXPECT_NAME;
-                hasName = false;
+                yield STATUS_OK; // First entry, no separator
+            case SCOPE_OBJECT:
+                if (!hasName) yield STATUS_EXPECT_NAME;
+                hasName = false; // Wrote value, new entry requires name again
                 hasEntry = true;
-                return STATUS_NEEDS_NAME_SEPARATOR;
-            }
-            case SCOPE_ARRAY -> {
-                boolean had = hasEntry;
+                yield STATUS_NEEDS_VALUE_SEPARATOR;
+            case SCOPE_ARRAY:
+                if (hasEntry) yield STATUS_NEEDS_VALUE_SEPARATOR;
                 hasEntry = true;
-                return had ? STATUS_NEEDS_VALUE_SEPARATOR : STATUS_OK;
-            }
-        }
-        return STATUS_OK;
+                yield STATUS_OK; // First entry, no separator
+            default:
+                yield STATUS_OK;
+        };
     }
 
     public byte writeNameStatus() {
