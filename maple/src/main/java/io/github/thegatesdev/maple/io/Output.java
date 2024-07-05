dd68dab3a49bef4374548e35c7994b5d623181e6
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-07-05 12:43:59 +0200

    Add Output javadocs

--- maple/src/main/java/io/github/thegatesdev/maple/io/Output.java
+++ maple/src/main/java/io/github/thegatesdev/maple/io/Output.java
@@ -4,18 +4,44 @@ import io.github.thegatesdev.maple.io.output.*;
 
 import java.io.*;
 
+/**
+ * Represents a way to output raw characters.
+ */
 public interface Output {
 
 
+    /**
+     * Get an output that writes data to a {@code Writer}.
+     *
+     * @param writer the write to use
+     * @return the new writer destination
+     */
     static Output writer(Writer writer) {
         return WriterOutput.create(writer);
     }
 
 
+    /**
+     * Output a single character.
+     *
+     * @param character the character to output
+     */
     void raw(int character);
 
+    /**
+     * Output the characters in the given string.
+     *
+     * @param string the string to output
+     */
     void raw(String string);
 
+    /**
+     * Outputs a portion of an array of characters.
+     *
+     * @param buffer the array of characters
+     * @param offset the offset from which to start outputting characters
+     * @param lenght the number of characters to output
+     */
     void raw(char[] buffer, int offset, int lenght);
 }
 
